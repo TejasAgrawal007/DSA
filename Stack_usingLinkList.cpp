351a0707fb9ff85bb6bee04cf6d1425239b8d8f2
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2025-02-25 16:13:46 +0530

    Create Stack_usingLinkList.cpp

---
+++ Stack_usingLinkList.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+class Stack{
+    
+    list<int> ll;
+    
+    public:
+        
+        void push(int val)
+        {
+            ll.push_front(val);
+        }
+        
+        int top()
+        {
+            return ll.front();
+        }
+        
+        void pop()
+        {
+            ll.pop_front();
+        }
+        
+        bool empty()
+        {
+            return ll.size() == 0;
+        }
+    
+};
+
+int main()
+{
+    
+    Stack s;
+    
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    
+    while(!s.empty())
+    {
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+    
+    return 0;
+}
